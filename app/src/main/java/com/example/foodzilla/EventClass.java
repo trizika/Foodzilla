8a6ce5c4e612f824207ab73ba52b4940fc9d11c4
michael-27 <teamlegmon@gmail.com>
2019-11-15 11:00:40 -0500

    //EventClass

---
+++ app/src/main/java/com/example/foodzilla/EventClass.java
@@ -0,0 +1,33 @@
+package com.example.foodzilla;
+
+public class EventClass {
+    String eventCreator;
+    String eventSignedUpUser;
+    Boolean eventFull;
+    Integer eventCapacity;
+    String eventLocation;
+    String eventFoodServed;
+    String eventStartDateAndTime;
+    String eventEndDateAndTime;
+    String eventLinkInfo;
+    String eventStatus;
+
+    public EventClass() {
+    }
+
+    public EventClass(String eventCreator, String eventSignedUpUser, Boolean eventFull, Integer eventCapacity, String eventLocation, String eventFoodServed, String eventStartDateAndTime, String eventEndDateAndTime, String eventLinkInfo, String eventStatus) {
+        this.eventCreator = eventCreator;
+        this.eventSignedUpUser = eventSignedUpUser;
+        this.eventFull = eventFull;
+        this.eventCapacity = eventCapacity;
+        this.eventLocation = eventLocation;
+        this.eventFoodServed = eventFoodServed;
+        this.eventStartDateAndTime = eventStartDateAndTime;
+        this.eventEndDateAndTime = eventEndDateAndTime;
+        this.eventLinkInfo = eventLinkInfo;
+        this.eventStatus = eventStatus;
+    }
+}
+
+
+
