e8d0eeaa95e81e3181d187a35231f7638bf9fb70
taliarizika <trizika@umich.edu>
2019-11-17 16:47:06 -0500

    coded main activity

--- app/src/main/java/com/example/foodzilla/MainActivity.java
+++ app/src/main/java/com/example/foodzilla/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.foodzilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 TextView textViewfoodzillamain;
 EditText editTextemailmain, editTextpasswordmain;
@@ -19,5 +20,24 @@ Button buttonLoginmain, buttonRegistermain;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     textViewfoodzillamain = findViewById(R.id.textVieweventdateedetails);
+     editTextemailmain = findViewById(R.id.editTextemailmain);
+     editTextpasswordmain = findViewById(R.id.editTextpasswordmain);
+     buttonLoginmain = findViewById(R.id.buttonLoginmain);
+     buttonRegistermain = findViewById(R.id.buttonRegistermain);
+
+     buttonRegistermain.setOnClickListener(this);
+     buttonLoginmain.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
