e5c0c46ab029f2daf3958b104d2048790cafdfe3
taliarizika <trizika@umich.edu>
2019-11-17 17:16:54 -0500

    coded main activity

--- app/src/main/java/com/example/foodzilla/MainActivity.java
+++ app/src/main/java/com/example/foodzilla/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.foodzilla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,7 +16,7 @@ TextView textViewfoodzillamain;
 EditText editTextemailmain, editTextpasswordmain;
 Button buttonLoginmain, buttonRegistermain;
 
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ Button buttonLoginmain, buttonRegistermain;
      buttonRegistermain.setOnClickListener(this);
      buttonLoginmain.setOnClickListener(this);
 
-
+ mAuth = FirebaseAuth.getInstance();
 
 
 
@@ -38,6 +40,32 @@ Button buttonLoginmain, buttonRegistermain;
 
     @Override
     public void onClick(View view) {
-        
+
+      String email = editTextemailmain.getText().toString();
+      String password = editTextpasswordmain.getText().toString();
+
+      if (view == buttonRegistermain){
+          mAuth.createUserWithEmailAndPassword(email, password)
+                  .addOnCompleteListener(this, (task) --> {
+          if (task.isSuccessful()) {
+              Toast.makeText(MainActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+              Intent mainIntent = new Intent(MainActivity.this, SpottingMap.class);
+          } else {
+              Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+          }
+
+          Intent landingIntent = new Intent(this, SpottingMap.class);
+          startActivity(landingIntent);
+
+
+          else if (view == buttonLoginmain) {
+              mAuth.signInWithEmailAndPassword(email, password)
+                      .addOnCompleteListener(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+          }
+          });
+
+          Intent landingintent = new Intent(this, SpottingMap.class);
+          startActivity(landingintent);
+
     }
 }
