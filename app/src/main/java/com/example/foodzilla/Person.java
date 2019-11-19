78a47c101adcd8b08d8371d09b8f6fbe43c2284d
sarnathc <sarnathc@umich.edu>
2019-11-19 13:34:24 -0500

    added person class

---
+++ app/src/main/java/com/example/foodzilla/Person.java
@@ -0,0 +1,24 @@
+package com.example.foodzilla;
+
+import android.widget.Button;
+
+public class Person {
+
+    String firstname;
+    String lastname;
+    String email;
+
+    public Person() {
+    }
+
+    public Person(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+
+
+    }
+}
+
+
+
