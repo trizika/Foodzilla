33755f83fa7d0a27456548dda8c23ffbaa0b9f47
williamzhangdesign <whzhang@umich.edu>
2019-11-21 14:03:35 -0500

    dqw

--- app/src/main/java/com/example/foodzilla/MyEvents.java
+++ app/src/main/java/com/example/foodzilla/MyEvents.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MyEvents extends AppCompatActivity {
 
+
+    button buttonNOTHINGMyEvents, button buttonSwitchAvailableEvents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
+
+        buttonNOTHINGMyEvents = findViewById(R.id.buttonMyEventsCr);
+
     }
 }
