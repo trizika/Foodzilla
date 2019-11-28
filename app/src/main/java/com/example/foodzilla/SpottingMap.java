76307e7cc868e7402ffaf44b69d731c6fda61b2d
taliarizika <trizika@umich.edu>
2019-11-28 10:29:24 -0800

    coded activity

--- app/src/main/java/com/example/foodzilla/SpottingMap.java
+++ app/src/main/java/com/example/foodzilla/SpottingMap.java
@@ -10,5 +10,6 @@ public class SpottingMap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotting_map);
+
     }
 }
--- app/src/main/java/com/example/foodzilla/Spottings.java
+++ app/src/main/java/com/example/foodzilla/Spottings.java
@@ -3,12 +3,45 @@ package com.example.foodzilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Spottings extends AppCompatActivity {
+public class Spottings extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextLocationDetailsReportSpotting;
+
+    Button buttonSubmitFoodReportSpotting;
+
+    ImageView imageViewSpottingPicReportSpotting;
+
+    TextView textViewReportFoodSpotting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spottings);
+
+        editTextLocationDetailsReportSpotting = findViewById(R.id.editTextLocationDetailsReportSpotting);
+        buttonSubmitFoodReportSpotting = findViewById(R.id.buttonSubmitFoodReportSpotting);
+        imageViewSpottingPicReportSpotting = findViewById(R.id.imageViewSpottingPicReportSpotting);
+        textViewReportFoodSpotting = findViewById(R.id.textViewReportFoodSpotting);
+
+        buttonSubmitFoodReportSpotting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSubmitFoodReportSpotting) {
+           // how do i getImage? instead of GetText
+            String spottingpic = imageViewSpottingPicReportSpotting
+           String spottinglocation = editTextLocationDetailsReportSpotting.getText().toString();
+
+
+            // submit that food spotting to the spotting database then return to spotting map page
+        }
+
     }
 }
