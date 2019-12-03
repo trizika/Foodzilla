0675dbd3b34b6f154ac91cdd7fa86dacc7184941
williamzhangdesign <whzhang@umich.edu>
2019-12-03 13:45:42 -0500

    dsds

--- app/src/main/java/com/example/foodzilla/MyEvents.java
+++ app/src/main/java/com/example/foodzilla/MyEvents.java
@@ -1,5 +1,6 @@
 package com.example.foodzilla;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MyEvents extends AppCompatActivity implements View.OnClickListener {
 
 
-    Button buttonNOTHINGme, buttonSwitchMenuAv, buttonEvent1me, buttonEvent2me, buttonEvent3me;
+    Button buttonNOTHINGme, buttonSwitchMenuAvme, buttonEvent1me, buttonEvent2me, buttonEvent3me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +18,21 @@ public class MyEvents extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_my_events);
 
         buttonNOTHINGme = findViewById(R.id.buttonMyEventsCr);
-        buttonSwitchMenuAv = findViewById(R.id.buttonAvailableEventsCr);
+        buttonSwitchMenuAvme = findViewById(R.id.buttonAvailableEventsCr);
         buttonEvent1me = findViewById(R.id.button1);
         buttonEvent2me = findViewById(R.id.button2);
         buttonEvent3me = findViewById(R.id.button3);
     }
+
     public void onClick(View view) {
-        
+        if (view == buttonEvent1me || view == buttonEvent2me || view == buttonEvent3me) {
+            Intent intent = new Intent(this, EventDetails.class);
+            startActivity(intent);
+        }
+        else if (view == buttonSwitchMenuAvme) {
+            Intent intent = new Intent(this, AvailableEvents.class);
+            startActivity(intent);
+        }
     }
 
 
