e1aa68002c001da96f7a6eb76281cc664f9d71c9
taliarizika <trizika@umich.edu>
2019-12-03 14:18:44 -0500

    coded activity

--- app/src/main/java/com/example/foodzilla/ReportSpotting.java
+++ app/src/main/java/com/example/foodzilla/ReportSpotting.java
@@ -2,6 +2,7 @@ package com.example.foodzilla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ReportSpoting extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ReportSpotting extends AppCompatActivity implements View.OnClickListener {
     EditText editTextLocationDetailsReportSpotting;
 
     Button buttonSubmitFoodReportSpotting;
@@ -34,6 +38,20 @@ public class ReportSpoting extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-//...
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        final DatabaseReference myref = database.getReference("Spottings");
+
+
+        FoodSpottingsClass spotting1 = new FoodSpottingsClass(editTextLocationDetailsReportSpotting.getText().toString());
+                //imageViewSpottingPicReportSpotting.set.toString());
+
+        myref.push().setValue(spotting1);
+
+
+        Intent intent1 = new Intent(ReportSpotting.this, SpottingMap.class);
+
+        startActivity(intent1);
     }
 }
