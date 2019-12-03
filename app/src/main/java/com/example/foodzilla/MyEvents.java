c8bd0bd12b7f81631ab0c7b08b2bea23b97c2094
williamzhangdesign <whzhang@umich.edu>
2019-12-03 13:22:29 -0500

    d3d2d

--- app/src/main/java/com/example/foodzilla/MyEvents.java
+++ app/src/main/java/com/example/foodzilla/MyEvents.java
@@ -1,23 +1,30 @@
 package com.example.foodzilla;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyEvents extends AppCompatActivity {
+public class MyEvents extends AppCompatActivity implements View.OnClickListener {
 
 
-    Button buttonNOTHINGMyEvents, buttonSwitchMenuAv;
+    Button buttonNOTHINGme, buttonSwitchMenuAv, buttonEvent1me, buttonEvent2me, buttonEvent3me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
 
-        buttonNOTHINGMyEvents = findViewById(R.id.buttonMyEventsCr);
+        buttonNOTHINGme = findViewById(R.id.buttonMyEventsCr);
         buttonSwitchMenuAv = findViewById(R.id.buttonAvailableEventsCr);
+        buttonEvent1me = findViewById(R.id.button1);
+        buttonEvent2me = findViewById(R.id.button2);
+        buttonEvent3me = findViewById(R.id.button3);
+    }
+    public void onClick(View view) {
+        
+    }
 
 
     }
-}
