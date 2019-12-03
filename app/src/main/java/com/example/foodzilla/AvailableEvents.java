301e6df9c9200c4c673b0f9c424c055cdf7a147c
sarnathc <sarnathc@umich.edu>
2019-12-03 13:54:08 -0500

    available events part 2

--- app/src/main/java/com/example/foodzilla/AvailableEvents.java
+++ app/src/main/java/com/example/foodzilla/AvailableEvents.java
@@ -3,8 +3,16 @@ package com.example.foodzilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AvailableEvents extends AppCompatActivity {
+
+
+
+    EditText date1, date2,date3, date3;
+
+    EditText name1, name2, name3, name4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
