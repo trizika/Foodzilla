2561b9e0df2160c3fcb48dcd4822c98341bf09a1
sarnathc <sarnathc@umich.edu>
2019-12-03 14:22:27 -0500

    editted the FoodSpottingsClass

--- app/src/main/java/com/example/foodzilla/FoodSpottingsClass.java
+++ app/src/main/java/com/example/foodzilla/FoodSpottingsClass.java
@@ -3,17 +3,13 @@ package com.example.foodzilla;
 public class FoodSpottingsClass {
     String location;
     Byte picture;
-    Boolean status;
-    String date_Time;
 
     public FoodSpottingsClass() {
     }
 
-    public FoodSpottingsClass(String location, Byte picture, Boolean status, String date_Time) {
+    public FoodSpottingsClass(String location, Byte picture) {
         this.location = location;
         this.picture = picture;
-        this.status = status;
-        this.date_Time = date_Time;
     }
 }
 
